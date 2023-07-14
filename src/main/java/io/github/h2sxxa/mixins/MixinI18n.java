63c1eae987f15894de33990013d15bc8279ef21a
IAXRetailer <h2sxxa0w0@gmail.com>
2023-07-15 07:51:50 +0800

    no overwrite ,use redirect

--- src/main/java/io/github/h2sxxa/mixins/MixinI18n.java
+++ src/main/java/io/github/h2sxxa/mixins/MixinI18n.java
@@ -1,8 +1,9 @@
 package io.github.h2sxxa.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import io.github.h2sxxa.Consts;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.resources.Locale;
@@ -10,15 +11,10 @@ import net.minecraft.client.resources.Locale;
 @Mixin(value=I18n.class,remap = Consts.IS_REMAP)
 public class MixinI18n {
     @Shadow
-    static Locale i18nLocale;
+    private static Locale i18nLocale;
 
-    @Overwrite
-    /**
-     * @author H2Sxxa
-     * @reason no need to inject it
-     * Translates the given string and then formats it. Equivilant to String.format(translate(key), parameters).
-     */
-    public static String format(String translateKey, Object... parameters){
+    @Redirect(method = "format",at = @At("INVOKE"))
+    private static String onformat(Locale slotLocale,String translateKey, Object... parameters){
         if (I18n.hasKey("touhoutweaks.".concat(translateKey))){
          return i18nLocale.formatMessage("touhoutweaks.".concat(translateKey), parameters);
         }else{
